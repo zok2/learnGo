c2bdc27dcf89321b122e5393d67d9bbafba547e5
zok2 <zok2@zok2deiMac-Pro.local>
2022-04-11 21:59:03 +0800

    爬楼梯

--- src/algorithm/climbStairs.go
+++ src/algorithm/climbStairs.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"math"
 )
-
+// 通项公式
 func climbStairs1(n int) int {
 	sqrt5 := math.Sqrt(5)
 	pow1 := math.Pow((1+sqrt5)/2, float64(n+1))
@@ -12,7 +12,7 @@ func climbStairs1(n int) int {
 	return int(math.Round((pow1 - pow2) / sqrt5))
 }
 
-// 通项公式
+
 func climbStairs2(n int) int {
 	p, q, r := 0, 0, 1
 	for i := 1; i <= n; i++ {
